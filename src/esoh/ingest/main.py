d7734b0dcbaf71180235920945097745a2a7d6d6
Amund Isaksen <amundi@met.no>
2023-09-20 14:43:13 +0200

    Updated input handling in ingestor ingress class

--- src/esoh/ingest/main.py
+++ src/esoh/ingest/main.py
@@ -4,6 +4,12 @@ from esoh.ingest.messages import load_files, build_message
 
 import xarray as xr
 
+from jsonschema import Draft202012Validator, ValidationError
+import json
+
+
+import logging
+
 
 class ingest_to_pipeline():
     """
@@ -11,13 +17,21 @@ class ingest_to_pipeline():
     Should accept paths or objects to pass on to the datastore and mqtt broker.
     """
 
-    def __init__(self, mqtt_conf: dict, uuid_prefix: str, testing: bool = False):
+    def __init__(self, mqtt_conf: dict,
+                 uuid_prefix: str,
+                 testing: bool = False,
+                 esoh_mqtt_schema="schemas/e-soh-message-spec.json"):
         self.uuid_prefix = uuid_prefix
 
         if testing:
             return
 
-        self.mqtt = mqtt_connection(mqtt_conf["host"], mqtt_conf["topic"])
+        self.mqtt = mqtt_connection(mqtt_conf["host"])
+
+        with open(esoh_mqtt_schema, "r") as file:
+            self.esoh_mqtt_schema = json.load(file)
+
+        self.scheam_validator = Draft202012Validator(self.esoh_mqtt_schema)
 
     def ingest_message(self, message: [str, object], input_type: str = None):
         if not input_type:
@@ -25,10 +39,23 @@ class ingest_to_pipeline():
 
         return self.build_message(message, input_type)
 
-    def decide_input_type(self):
-        pass
+    def decide_input_type(self, message):
+        if isinstance(message, str):
+            match message.split(".")[-1]:
+                case "nc":
+                    return "netCDF"
+                case "bufr":
+                    return "bufr"
+                case _:
+                    raise ValueError("Unknown filetype provided.")
 
     def build_messages(self, message: [str, object], input_type: str = None):
+        if not input_type:
+            if isinstance(message, str):
+                input_type = self.decide_input_type()
+            else:
+                raise TypeError(
+                    "Illegal use, can not provide objects without specifying input_type")
         match input_type:
             case "netCDF":
                 if isinstance(message, str):
@@ -43,5 +70,13 @@ class ingest_to_pipeline():
             case "bufr":
                 raise NotImplementedError("Handeling of bufr not implemented")
 
-    def publish_messages(self):
-        pass
+    def publish_messages(self, messages: list):
+        for msg in messages:
+            try:
+                self.scheam_validator.validate(msg)
+                self.mqtt.send_message(msg)
+            except ValidationError as v_error:
+                logging.warning("Message did not pass schema validation, " + v_error)
+                continue
+            except Exception as e:
+                raise e
