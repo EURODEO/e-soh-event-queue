3ea44c18e8e86d33ac479f60f36afd93bd95d421
Amund Isaksen <amundi@met.no>
2023-09-21 09:16:03 +0200

    Updated input handeling

--- src/esoh/ingest/main.py
+++ src/esoh/ingest/main.py
@@ -1,8 +1,5 @@
 from esoh.ingest.send_mqtt import mqtt_connection
-from esoh.ingest.messages import load_files, build_message
-
-
-import xarray as xr
+from esoh.ingest.messages import messages
 
 from jsonschema import Draft202012Validator, ValidationError
 import json
@@ -33,42 +30,11 @@ class ingest_to_pipline():
 
         self.scheam_validator = Draft202012Validator(self.esoh_mqtt_schema)
 
-    def ingest_message(self, message: [str, object], input_type: str = None):
+    def ingest(self, message: [str, object], input_type: str = None):
         if not input_type:
             input_type = self.decide_input_type(message)
 
-        return self.build_message(message, input_type)
-
-    def decide_input_type(self, message):
-        if isinstance(message, str):
-            match message.split(".")[-1]:
-                case "nc":
-                    return "netCDF"
-                case "bufr":
-                    return "bufr"
-                case _:
-                    raise ValueError("Unknown filetype provided.")
-
-    def build_messages(self, message: [str, object], input_type: str = None):
-        if not input_type:
-            if isinstance(message, str):
-                input_type = self.decide_input_type()
-            else:
-                raise TypeError(
-                    "Illegal use, can not provide objects without specifying input_type")
-        match input_type:
-            case "netCDF":
-                if isinstance(message, str):
-                    return load_files(message, input_type=input_type, uuid_prefix=self.uuid_prefix)
-                elif isinstance(message, xr.Dataset):
-                    return build_message(message,
-                                         input_type=input_type,
-                                         uuid_prefix=self.uuid_prefix)
-                else:
-                    raise TypeError("Unknown netCDF type, expected path"
-                                    + f"or xarray.Dataset, got {type(message)}")
-            case "bufr":
-                raise NotImplementedError("Handeling of bufr not implemented")
+        self.build_message(message, input_type)
 
     def publish_messages(self, messages: list):
         for msg in messages:
@@ -80,3 +46,22 @@ class ingest_to_pipline():
                 continue
             except Exception as e:
                 raise e
+
+    def _decide_input_type(self, message):
+        if isinstance(message, str):
+            match message.split(".")[-1]:
+                case "nc":
+                    return "netCDF"
+                case "bufr":
+                    return "bufr"
+                case _:
+                    raise ValueError(f"Unknown filetype provided. Got {message.split('.')[-1]}")
+
+    def _build_messages(self, message: [str, object], input_type: str = None):
+        if not input_type:
+            if isinstance(message, str):
+                input_type = self._decide_input_type(message)
+            else:
+                raise TypeError("Illagel usage, not allowed to input"
+                                + "objects without specifying input type")
+        return messages(message, input_type, self.uuid_prefix)
