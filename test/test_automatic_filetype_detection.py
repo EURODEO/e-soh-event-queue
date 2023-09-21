1766846f9beb193c2db222b70ad1c6c9f22ed41d
Amund Isaksen <amundi@met.no>
2023-09-21 09:16:25 +0200

    Implement test for filetype detection

---
+++ test/test_automatic_filetype_detection.py
@@ -0,0 +1,15 @@
+import pytest
+
+from esoh.ingest.main import ingest_to_pipline
+import glob
+
+
+@pytest.mark.parametrize("test_inpt, expected", [('test/test_data/knmi/20230101.nc', 'netCDF'),
+                                                 ('test/test_data/knmi/20230102.nc', 'netCDF'),
+                                                 ('test/test_data/knmi/20221231.nc', 'netCDF'),
+                                                 ('test/test_data/met_norway/'
+                                                 + 'air_temperature_gullingen_skisenter-parent.nc',
+                                                  'netCDF')])
+def test_filetype_detection(test_inpt, expected):
+    msg_build = ingest_to_pipline(None, "testing", testing=True)
+    assert msg_build._decide_input_type(test_inpt) == expected
