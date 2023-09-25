793582cd47ec9a6b01db06a0cc0f0bdae3d0deb8
Amund Isaksen <amundi@met.no>
2023-09-25 12:12:11 +0200

    merged fix-uuid in to branch

--- src/esoh/ingest/main.py
+++ src/esoh/ingest/main.py
@@ -8,6 +8,12 @@ import json
 import logging
 
 
+<<<<<<< HEAD
+=======
+logger = logging.getLogger(__name__)
+
+
+>>>>>>> c68117b (Fixed more typos)
 class ingest_to_pipeline():
     """
     This class should be the main interaction with this python package.
@@ -28,40 +34,57 @@ class ingest_to_pipeline():
         with open(esoh_mqtt_schema, "r") as file:
             self.esoh_mqtt_schema = json.load(file)
 
-        self.scheam_validator = Draft202012Validator(self.esoh_mqtt_schema)
+        self.schema_validator = Draft202012Validator(self.esoh_mqtt_schema)
 
     def ingest(self, message: [str, object], input_type: str = None):
+        """
+        Method designed to be main interaction point with this package.
+        Will interpret call all methods for deciding input type, build the mqtt messages, and
+        publish them.
+
+        """
         if not input_type:
             input_type = self.decide_input_type(message)
 
-        self.build_message(message, input_type)
+        self.publish_messages(self._build_message(message, input_type))
 
     def publish_messages(self, messages: list):
+        """
+        This method accepts a list of json strings ready to be published to the mqtt topic.
+        """
         for msg in messages:
             try:
-                self.scheam_validator.validate(msg)
+                self.schema_validator.validate(msg)
                 self.mqtt.send_message(msg)
             except ValidationError as v_error:
-                logging.warning("Message did not pass schema validation, " + v_error)
+                logging.error("Message did not pass schema validation, " + v_error)
                 continue
             except Exception as e:
                 raise e
 
-    def _decide_input_type(self, message):
-        if isinstance(message, str):
-            match message.split(".")[-1]:
-                case "nc":
-                    return "netCDF"
-                case "bufr":
-                    return "bufr"
-                case _:
-                    raise ValueError(f"Unknown filetype provided. Got {message.split('.')[-1]}")
+    def _decide_input_type(self, message) -> str:
+        """
+        Internal method for deciding what type of input is being provided.
+        """
+        match message.split(".")[-1]:
+            case "nc":
+                return "netCDF"
+            case "bufr":
+                return "bufr"
+            case _:
+                logger.critical(f"Unknown filetype provided. Got {message.split('.')[-1]}")
+                raise ValueError(f"Unknown filetype provided. Got {message.split('.')[-1]}")
 
-    def _build_messages(self, message: [str, object], input_type: str = None):
+    def _build_messages(self, message: [str, object], input_type: str = None) -> list:
+        """
+        Internal method for calling the message building.
+        """
         if not input_type:
             if isinstance(message, str):
                 input_type = self._decide_input_type(message)
             else:
-                raise TypeError("Illagel usage, not allowed to input"
+                logger.critical("Illegal usage, not allowed to input"
+                                + "objects without specifying input type")
+                raise TypeError("Illegal usage, not allowed to input"
                                 + "objects without specifying input type")
         return messages(message, input_type, self.uuid_prefix)
