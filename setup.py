8039b582dea329ffaffc6a74dde030d0b4eaed37
Amund Isaksen <amundi@met.no>
2023-10-12 07:23:01 +0200

    Started setup, metadata bug in pyproject

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from pybind11.setup_helpers import Pybind11Extension
 
 ext_modules = [
     Pybind11Extension(
-        "esoh.ingest.bufr.esoh",
+        "esoh.ingest.bufr",
         sorted(glob("src/esoh/ingest/bufr/*.cpp")),  # Sort source files for reproducibility
+        extra_compile_args=["-std=c++17"]
     ),
 ]
